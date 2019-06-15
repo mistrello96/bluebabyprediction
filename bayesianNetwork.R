546366469f3fc55f364df6dcc224af3aa7f7f073
Matteo Mistri <matteo.mistri1996@gmail.com>
2019-06-15 15:46:56 +0200

    Added performance measure fixed for multi class problem, also added boxplot and mean/sd of each measure. Created structure for pdf save of the images of the network, still need to find a way to fit them into a page

--- bayesianNetwork.R
+++ bayesianNetwork.R
@@ -1,12 +1,19 @@
 library(bnlearn)
+
+# funzione che valuta le performance medie al termine della 10-fold cross validation
+evaluatePerformance = function(accuracy, precision, recall, f1measure){
+  print(paste("Accuracy mean:",  mean(accuracy)))
+  print(paste("Accuracy sd:",  sd(accuracy)))
+  print(paste("Precision mean:",  mean(precision)))
+  print(paste("Precision sd:",  sd(precision)))
+  print(paste("Recall mean:", mean(recall)))
+  print(paste("Recall sd:", sd(recall)))
+  print(paste("F1measure mean:", mean(f1measure)))
+  print(paste("F1measure sd:", sd(f1measure)))
+}
+
 #load file
 MyData <- read.csv(file="BlueBaby.csv", header=TRUE, sep=",")
-
-#x = MyData[MyData$BirthAsphyxia == "yes",]
-#y = MyData[MyData$BirthAsphyxia == "no",]
-#y = y[sample(nrow(y)), ]
-#z = y[0:1028,]
-#MyData = merge(x,z, all=TRUE)
 
 dataset <- MyData[sample(nrow(MyData)), ] 
 ind <- cut(1:nrow(dataset), breaks = 10, labels = F)
@@ -53,25 +60,78 @@ for(i in 1:10){
   
   # prediction
   paper_prediction = predict(paper_fit, "Disease", testset_cropped, method = "bayes-lw")
+  induced_prediction = predict(induced_fit, "Disease", testset_cropped, method = "bayes-lw")
   
-  # performance evaluation
+  # performance evaluation paper
   confusion.matrix = table(testset$Disease, paper_prediction)
   accuracy = sum(diag(confusion.matrix))/sum(confusion.matrix)
   paper_accuracy = append(accuracy, paper_accuracy)
-  precision = confusion.matrix[1,1] / (confusion.matrix[1,1] + confusion.matrix[2,1])
+  precision = 0
+  recall = 0
+  for (i in 1:6){
+    precision = precision + (confusion.matrix[i,i] / sum(confusion.matrix[,i]))
+    recall = recall + (confusion.matrix[i,i] / sum(confusion.matrix[i,]))
+    
+  }
+  precision = precision / 6
+  recall = recall / 6
   paper_precision = append(precision, paper_precision)
-  recall = confusion.matrix[1,1] / (confusion.matrix[1,1] + confusion.matrix[1,2])
   papar_recall = append(recall, papar_recall)
   f1measure = 2 * (precision * recall / (precision + recall))
   paper_f1measure = append(f1measure, paper_f1measure)
+  
+  # performance evaluation induced
+  confusion.matrix = table(testset$Disease, induced_prediction)
+  accuracy = sum(diag(confusion.matrix))/sum(confusion.matrix)
+  induced_accuracy = append(accuracy, induced_accuracy)
+  precision = 0
+  recall = 0
+  for (i in 1:6){
+    precision = precision + (confusion.matrix[i,i] / sum(confusion.matrix[,i]))
+    recall = recall + (confusion.matrix[i,i] / sum(confusion.matrix[i,]))
+    
   }
+  precision = precision / 6
+  recall = recall / 6
+  induced_precision = append(precision, induced_precision)
+  induced_recall = append(recall, induced_recall)
+  f1measure = 2 * (precision * recall / (precision + recall))
+  induced_f1measure = append(f1measure, induced_f1measure)
+}
 
-# plots
+# Performance plot
+pdf("paper_performance.pdf", width = 8, height = 8)
+# boxplot delle misure di performance
+par(mfrow=c(2,2))
+boxplot(paper_accuracy, main = "Accuracy")
+boxplot(paper_precision, main = "Precision")
+boxplot(papar_recall, main = "Recall")
+boxplot(paper_f1measure, main = "F1Measure")
+dev.off()
+print("Evaluating paper performance:")
+evaluatePerformance(paper_accuracy, paper_precision, papar_recall, paper_f1measure)
+
+pdf("induced_performance.pdf", width = 8, height = 8)
+# boxplot delle misure di performance
+par(mfrow=c(2,2))
+boxplot(induced_accuracy, main = "Accuracy")
+boxplot(induced_precision, main = "Precision")
+boxplot(induced_recall, main = "Recall")
+boxplot(induced_f1measure, main = "F1Measure")
+dev.off()
+print("Evaluating induced performance:")
+evaluatePerformance(induced_accuracy, induced_precision, induced_recall, induced_f1measure)
+
+# plots of network
+#pdf("induced_fitted.pdf", width = 20, height = 20)
+graphviz.chart(induced_fit, grid = TRUE, main = "Rappresentazione della rete fittata")
+#dev.off()
+#pdf("paper_fitted.pdf", width = 15, height = 14)
 graphviz.chart(paper_fit, grid = TRUE, main = "Rappresentazione della rete fittata")
+#dev.off()
+#pdf("paper_structure.pdf", width = 5, height = 5)
 graphviz.plot(paper_model, main = "Representation of the paper network structure")
+#dev.off()
+#pdf("induced_structure.pdf", width = 5, height = 5)
 graphviz.plot(induced_model, main = "Representation of the inducded network structure")
-
-# debug prints
-paper_prediction
-testset$Disease
-confusion.matrix
+#dev.off()
