b50cadf994d01be6a0136ac7b6e8153396306590
Papetz <d.papetti1@campus.unimib.it>
2019-06-17 14:26:10 +0200

    added Demo and minors to networks plot

--- Demo/app.R
+++ Demo/app.R
@@ -1,6 +1,9 @@
+# Here the code is run once at the launchof the app, so put HERE all 
+# the instruction to import libries, datas, whatever must be run 
+# once in order to not affect performances
 library(shiny)
 
-# Define UI for app that draws a histogram ----
+# Define UI for app
 ui <- fluidPage(
   titlePanel(h1("This is a title")),
   
@@ -8,7 +11,15 @@ ui <- fluidPage(
     # position = right to put the sidebar on the right
     
     sidebarPanel( # side bar content
-      "sidebar panel"),
+      "sidebar panel",
+      fluidRow(
+        column(12, 
+               selectInput("select", h3("Select box"), 
+                           choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                          "Choice 3" = 3), selected = 1)
+        )
+      )
+    ),
       
     mainPanel(  # main panel content
       h1("First level title"), # , align = "center" to center the text
@@ -18,17 +29,19 @@ ui <- fluidPage(
       h5("Fifth level title"),
       h6("Sixth level title")
       
-      # look lesson 2 for formatted text and insert images
-      
-      
+      # look lesson 2 for formatted text and how insert images
+
+          
       
     )
   )
 )
 
-# Define server logic required to draw a histogram ----
+# Define server logic
 server <- function(input, output) {
-
+  # output$<id_widget_name> <- <render_funciont>({<a complicated function>})
+  # input$<id_widget_name> to access the value of the widget
 }
 
+# Run the app
 shinyApp(ui = ui, server = server)
